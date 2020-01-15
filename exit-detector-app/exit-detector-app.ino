2dbaf00905b7725a7721cc9902f79f03c80a670c
allen-n <allennikka@gmail.com>
2020-01-15 10:08:24 -0800

    Modified constants for real-world run.
    * Modified timers and distances for particular use case.

--- exit-detector-app/exit-detector-app.ino
+++ exit-detector-app/exit-detector-app.ino
@@ -8,41 +8,70 @@ const int echoPin1 = 13;
 UltraSonicDistanceSensor dSensor1(triggerPin1, echoPin1);
 
 // Distance Sensor 2
-const int triggerPin2 = 11;
-const int echoPin2 = 10;
-UltraSonicDistanceSensor dSensor2(triggerPin2, echoPin2);
+// const int triggerPin2 = 11;
+// const int echoPin2 = 10;
+// UltraSonicDistanceSensor dSensor2(triggerPin2, echoPin2);
+
+// Constants
+const int buffSize = 40;
+const int relayPin = 8;
 
 // Ring buffers
-RingBuffer buff1(10);
-RingBuffer buff2(10);
+RingBuffer buff1(buffSize);
+// RingBuffer buff2(10);
 
 // Target Distance Debouncing
-const double targetDist = 5.2;
+const double targetDist = 5;
 const double threshold = 0.30;
-DistDebouncer db(targetDist, threshold, 1000);
+const int dbTime = 4000; // Debounce time in ms
+
+DistDebouncer db(targetDist, threshold, dbTime);
 
 void setup()
 {
-  Serial.begin(9600); // We initialize serial connection so that we could print values from sensor.
-  Serial.println("Applicationg Running!");
+  Serial.begin(9600);
+  Serial.println("Application Running!");
+  pinMode(relayPin, OUTPUT);
 }
 
-
+// Delta time variables for main loop
 const unsigned long dt = 100;
 unsigned long prevMillis = 0;
+const double cmToFeet = 0.0328084; // Conversion from cm to feet
 
 void loop()
 {
-  // put your main code here, to run repeatedly :
+
   unsigned long currentMillis = millis();
   if (currentMillis - prevMillis > dt)
   {
+    // Update main delta time loop
     prevMillis = currentMillis;
-    double dist1 = 0.0328084 * dSensor1.measureDistanceCm(); // Convert to feet
+
+    // Read and buffer sensor 1
+    double dist1 = cmToFeet * dSensor1.measureDistanceCm(); // Convert to feet
     buff1.addValue(dist1);
-    double dist2 = 0.0328084 * dSensor2.measureDistanceCm(); // Convert to feet
-    buff2.addValue(dist2);
+
+    // Read and buffer sensor 2
+    // double dist2 = cmToFeet * dSensor2.measureDistanceCm(); // Convert to feet
+    // buff2.addValue(dist2);
+
+    // Determine if sensor 1 is in target range
     bool isTarget = db.isAtTarget(buff1.getMean());
-    Serial.println("D1: " + String(buff1.getMean()) + ", ~=3: " + String(isTarget) + ", D2: " + String(buff2.getMean()));
+    
+    // Two sensor print statement
+    // Serial.println("D1: " + String(buff1.getMean()) + ", ~=3: " + String(isTarget) + ", D2: " + String(buff2.getMean()));
+    
+    // One sensor print statement
+    Serial.println("D1: " + String(buff1.getMean()) + ", @target(" + String(targetDist) + "): " + String(isTarget));
+
+    // Relay activation
+    if (isTarget)
+    {
+
+      digitalWrite(relayPin, HIGH);
+      delay(100);
+      digitalWrite(relayPin, LOW);
+    }
   }
 }
